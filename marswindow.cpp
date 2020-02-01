aa5dd57434e897599c87ad4311df4f310af987f1
Rei-Ruimin <60533945+Rei-Ruimin@users.noreply.github.com>
2020-01-31 22:18:51 -0700

    Create marswindow.cpp

---
+++ marswindow.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+    if(year == 2018) {cout << "yes";return 0;}
+    int start_m = (year - 2018 -1)*12 + 8;
+    bool mars_year = false;
+    for(int i=1; i <=12; i++) {
+        if((start_m+i)%26 == 0) {
+            mars_year = true;
+            break;
+        }
+    }
+    if(mars_year) {cout <<"yes";}
+    else {cout << "no";}
+
+    return 0;
+}
