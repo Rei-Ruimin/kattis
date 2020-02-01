7693ac7dc146bff2c972f6b39a34b0d8a77a7fd5
Rei-Ruimin <60533945+Rei-Ruimin@users.noreply.github.com>
2020-01-31 22:14:37 -0700

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,10 @@
+numl = list(map(int,input().split()))
+numl.sort()
+string = input()
+for i in range(3):
+    if string[i] == 'A':
+        print(numl[0], end = ' ')
+    elif string[i] == 'B':
+        print(numl[1], end = ' ')
+    else:
+        print(numl[2], end = ' ')
