c5e636a42e39d5a2653b6282dfdc5daa6feae492
Rei-Ruimin <60533945+Rei-Ruimin@users.noreply.github.com>
2020-01-31 22:22:24 -0700

    Create sumoftheothers.cpp
    2020.1.30

---
+++ sumoftheothers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    while(getline(cin, s )) {
+       istringstream iss(s);
+       int num;
+       int sum = 0;
+       while(iss >> num) {
+           sum+=num;
+       }
+       cout<< sum/2 << endl;
+    }
+}
