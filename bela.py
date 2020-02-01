0890a43c19567d01c8da646cbd35158819ae1333
Rei-Ruimin <60533945+Rei-Ruimin@users.noreply.github.com>
2020-01-31 22:17:48 -0700

    Create bela.py

---
+++ bela.py
@@ -0,0 +1,12 @@
+N,B = map(str,input().split())
+N = int(N)
+dom = {'A':11,'K':4,'Q':3,'J':20,'T':10,'9':14,'8':0,'7':0}
+indom = {'A':11,'K':4,'Q':3,'J':2,'T':10,'9':0,'8':0,'7':0}
+pt = 0
+for i in range(0,4*N):
+    string = input()
+    if string[1] == B:
+        pt += dom[string[0]]
+    else:
+        pt += indom[string[0]]
+print(pt)
