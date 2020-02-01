fc6fd8253893155ae4d9728be4797a74ad2dfd19
Rei-Ruimin <60533945+Rei-Ruimin@users.noreply.github.com>
2020-01-31 22:25:27 -0700

    Create vauvau.cpp
    2020.1.13

---
+++ vauvau.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void check(int n, int A, int B, int C, int D) {
+    int count = 0;
+    if (0 < n%(A+B)&&n%(A+B)<=A) {
+        count++;
+    }
+    if (0< n%(C+D) && n%(C+D) <= C) {
+        count++;
+    }
+    if(count == 0) {
+        cout << "none" << endl;
+    }
+    else if(count == 1){
+        cout << "one" << endl;
+    }
+    else {
+        cout << "both" << endl;
+    }
+    
+}
+int main()
+{
+    int A,B,C,D,X,Y,Z;
+    cin >> A >> B>>C>>D>>X>>Y>>Z;
+    check(X,A,B,C,D);
+    check(Y,A,B,C,D);
+    check(Z,A,B,C,D);
+
+    return 0;
+}
