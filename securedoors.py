5209e97579e59510af8cdafb4db16819b28c5293
Rei-Ruimin <60533945+Rei-Ruimin@users.noreply.github.com>
2020-01-31 22:21:47 -0700

    Create securedoors.py
    2019.9.17

---
+++ securedoors.py
@@ -0,0 +1,21 @@
+n = int(input())
+memberlist = {}
+for i in range(0,n):
+    string = input().split()
+    name = string[1]
+    act = string[0]
+    if act == 'entry':
+        if name not in memberlist.keys():
+            print(name,'entered')
+            memberlist[name] = 1
+        elif memberlist[name] == 0:
+            print(name,'entered')
+            memberlist[name] += 1
+        else:
+            print(name,'entered (ANOMALY)')
+    else:
+        if name in memberlist.keys() and memberlist[name] > 0:
+            print(name,'exited')
+            memberlist[name] = 0
+        else:
+            print(name,'exited (ANOMALY)')
